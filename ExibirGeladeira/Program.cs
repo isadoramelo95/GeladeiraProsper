535cd9192f44bf7b1321c48d806f53348bb6d8cb
Isadora Melo <50156679+isadoramelo95@users.noreply.github.com>
2024-08-19 18:33:57 -0300

    Update Program.cs

--- ExibirGeladeira/Program.cs
+++ ExibirGeladeira/Program.cs
@@ -1,12 +1,13 @@
-﻿using Domain;
+using GeladeiraCodeRDIVersity;
+using System;
 
 Geladeira objGeladeira = new Geladeira();
 
 // andar 0 - Hortifruti
-var objItemMorango = new Item() { Alimento = "Morango", Quantidade = 1, Unidade = "Unidade", Classificacao = "Hotfruti", Id = 1 };
-var objItemUva = new Item() { Alimento = "Uva", Quantidade = 1, Unidade = "Cacho", Classificacao = "Hotfruti", Id = 2 };
-var objItemTomate = new Item() { Alimento = "Tomate", Quantidade = 1, Unidade = "", Classificacao = "Hotfruti", Id = 3 };
-var objItemBanana = new Item() { Alimento = "Banana", Quantidade = 1, Unidade = "Penca", Classificacao = "Hotfruti", Id = 4 };
+var objItemMorango = new Item() { Alimento = "Morango", Quantidade = 1, Unidade = "Unidade", Classificacao = "Hortifruti", Id = 1 };
+var objItemUva = new Item() { Alimento = "Uva", Quantidade = 1, Unidade = "Cacho", Classificacao = "Hortifruti", Id = 2 };
+var objItemTomate = new Item() { Alimento = "Tomate", Quantidade = 1, Unidade = "", Classificacao = "Hortifruti", Id = 3 };
+var objItemBanana = new Item() { Alimento = "Banana", Quantidade = 1, Unidade = "Penca", Classificacao = "Hortifruti", Id = 4 };
 
 // andar 1 - Laticínios e Enlatados
 var objItemTomatePelado = new Item() { Alimento = "Tomate Pelado", Quantidade = 1, Unidade = "Lata", Classificacao = "Laticínios e Enlatados", Id = 5 };
@@ -18,14 +19,34 @@ var objItemBacon = new Item() { Alimento = "Bacon", Quantidade = 1, Unidade = "K
 var objItemCarne = new Item() { Alimento = "Carne", Quantidade = 1, Unidade = "kilos", Classificacao = "Charcutaria, Carnes e Ovos", Id = 9 };
 var objItemOvos = new Item() { Alimento = "Ovos", Quantidade = 1, Unidade = "duzia", Classificacao = "Charcutaria, Carnes e Ovos", Id = 10 };
 
-//
-objGeladeira.AdicionarItensAoContainer(0, 0, objItemBanana);
-objGeladeira.AdicionarItensAoContainer(1, 0, objItemCremeLeite);
-objGeladeira.AdicionarItensAoContainer(0, 3, objItemCarne);
+//Adicionar os itens na geladeira
+//Hortifruti
+objGeladeira.AdicionarItemGeladeira(0, 0, 0, objItemMorango);
+objGeladeira.AdicionarItemGeladeira(0, 0, 1, objItemUva);
+objGeladeira.AdicionarItemGeladeira(0, 1, 2, objItemTomate);
 
-//
+// Laticínios e Enlatados
+objGeladeira.AdicionarItemGeladeira(1, 0, 0, objItemTomatePelado);
+objGeladeira.AdicionarItemGeladeira(1, 1, 1, objItemCremeLeite);
+objGeladeira.AdicionarItemGeladeira(1, 1, 2, objItemLeite);
+
+// Charcutaria, Carnes e Ovos
+objGeladeira.AdicionarItemGeladeira(2, 0, 0, objItemBacon);
+objGeladeira.AdicionarItemGeladeira(2, 0, 1, objItemCarne);
+objGeladeira.AdicionarItemGeladeira(2, 1, 3, objItemOvos);
+
+Console.WriteLine("Alimentos na geladeira:");
 objGeladeira.ExibirItens();
 
+objGeladeira.RemoverItem(1, 0, 0);
+
+var objItemIogurte = new Item() { Alimento = "Iogurte", Quantidade = 50, Unidade = "ml", Classificacao = "Laticínios e Enlatados", Id = 11 };
+objGeladeira.AdicionarItemGeladeira(1, 0, 0, objItemIogurte);
+
+Console.WriteLine();
+
+Console.WriteLine("Atualização da geladeira:");
+objGeladeira.ExibirItens();
 
 
 
